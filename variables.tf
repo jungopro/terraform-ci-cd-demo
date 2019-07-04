244199d3ee9629eb2df150e7f80e9aec60226995
omer <omerb@codevalue.net>
2019-07-04 12:13:48 +0300

    moved authentication out of configuration

--- variables.tf
+++ variables.tf
@@ -2,22 +2,6 @@
 ## General Init Variables ##
 ############################
 
-variable "subscription_id" {
-  description = "Azure Subscription. Define via enviroment variable TF_VAR_subscription_id = <YOUR_SUBSCRIPTION_ID>"
-}
-
-variable "tenant_id" {
-  description = "Azure Subscription. Define via enviroment variable TF_VAR_tenant_id = <YOUR_TENANT_ID>"
-}
-
-variable "client_id" {
-  description = "Azure Subscription. Define via enviroment variable TF_VAR_client_id = <YOUR_CLIENT_ID>"
-}
-
-variable "client_secret" {
-  description = "Azure Subscription. Define via enviroment variable TF_VAR_client_secret = <YOUR_CLIENT_SECRET>"
-}
-
 variable "location" {
   description = "Azure location to create the resource in"
 }
