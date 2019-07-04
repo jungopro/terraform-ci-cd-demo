0be2b0140c3bd34feacec0caaf27995548b4a3e0
omer <omerb@codevalue.net>
2019-07-04 10:09:07 +0300

    initial code to create rg in azure

---
+++ init.tf
@@ -0,0 +1,26 @@
+## Values to initialize the environment. No resource Creation
+
+provider "azurerm" {
+  version = "~> 1.2"
+
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
+
+provider "random" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+terraform {
+  backend "azurerm" {}
+}
