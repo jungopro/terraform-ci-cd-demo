b0e039fd697e55bdbed738ffab34d552c9476911
omer <omerb@codevalue.net>
2019-07-04 16:29:19 +0300

    modify init

--- init.tf
+++ init.tf
@@ -2,6 +2,11 @@
 
 provider "azurerm" {
   version = "~> 1.2"
+  
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 provider "random" {
