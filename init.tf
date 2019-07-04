c47b604b11384748883f85686660d275e2c83f06
omer <omerb@codevalue.net>
2019-07-04 16:30:16 +0300

    matched to v12

--- init.tf
+++ init.tf
@@ -3,10 +3,10 @@
 provider "azurerm" {
   version = "~> 1.2"
   
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 provider "random" {
