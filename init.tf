15fed4065988001bde5d875c96f24169a99beb67
omer <omerb@codevalue.net>
2019-11-29 22:41:19 +0200

    modified k8s and helm provider login

--- init.tf
+++ init.tf
@@ -28,8 +28,6 @@ provider "local" {
 provider "kubernetes" {
   version                = "~> 1.8"
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
-  username               = azurerm_kubernetes_cluster.aks.kube_config.0.username
-  password               = azurerm_kubernetes_cluster.aks.kube_config.0.password
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
@@ -41,18 +39,13 @@ provider "helm" {
   namespace        = "kube-system"
   service_account  = kubernetes_service_account.tiller_sa.metadata.0.name
   install_tiller   = true
-  
 
   kubernetes {
     host     = azurerm_kubernetes_cluster.aks.kube_config.0.host
-    username = azurerm_kubernetes_cluster.aks.kube_config.0.username
-    password = azurerm_kubernetes_cluster.aks.kube_config.0.password
 
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-
-    load_config_file = false
   }
 }
 
