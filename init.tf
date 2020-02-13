335924cc5c91d03c8955a4d15b6deb719f890758
omer <omerb@codevalue.net>
2020-02-13 20:44:56 +0200

    pinned provider version

--- init.tf
+++ init.tf
@@ -26,12 +26,12 @@ provider "local" {
 }
 
 provider "kubernetes" {
-  version                = "~> 1.8"
+  version                = "1.10.0"
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-  load_config_file       = "false"
+  load_config_file       = false
 }
 
 provider "helm" {
@@ -46,7 +46,7 @@ provider "helm" {
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-    load_config_file       = "false"
+    load_config_file       = false
   }
 }
 
